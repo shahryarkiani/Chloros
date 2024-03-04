a7d89d03129310d16a9b15ae0d13c505dfd81cf4
Shahryar Kiani <shahryarkiani04@gmail.com>
2024-03-03 21:35:41 -0500

    Add unit test for ping-pong with arguments

--- test/src/argument_tests.c
+++ test/src/argument_tests.c
@@ -22,6 +22,36 @@ static bool arg_test() {
   return true;
 }
 
+static const int ITER_NUM = 5;
+
+static int ID_NUM = 0;
+
+static int ping_pong_arg(void *arg) {
+  volatile int *check = (int *) arg;
+
+  int myid = ID_NUM++;
+  *check = myid;
+  
+  for(int i = 0; i < ITER_NUM; i++) {
+    while(*check == myid);
+    *check = myid;
+  }
+
+  return 0;
+}
+
+static bool ping_pong_arg_test(){
+  volatile int num = 0;
+
+  grn_init(true);
+  grn_spawn(ping_pong_arg,(void *) &num);
+  grn_spawn(ping_pong_arg,(void *) &num);
+  grn_wait();
+
+  return true;
+}
+
 BEGIN_TEST_SUITE(argument_tests) {
   run_test(arg_test);
+  run_test(ping_pong_arg_test);
 }
